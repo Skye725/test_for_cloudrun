d8c640ec2cfc98535b572eb68f883c2ed04e9c3f
Skye725 <101329664+Skye725@users.noreply.github.com>
2023-08-17 19:30:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
-# from flask_ngrok import run_with_ngrok
+# from flask import Flask
+# # from flask_ngrok import run_with_ngrok
 
-app = Flask(__name__)
-# run_with_ngrok(app)
+# app = Flask(__name__)
+# # run_with_ngrok(app)
 
-@app.route("/<name>")
- def home(name):
-    return f"<h1>hello {name}</h1>"
+# @app.route("/<name>")
+#  def home(name):
+#     return f"<h1>hello {name}</h1>"
 
-app.run()
+# app.run()
+a={'hello world'}
+return a
