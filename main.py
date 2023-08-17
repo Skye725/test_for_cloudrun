7257b9c42265023109e3dd205cdb6c0e769b97c5
Skye725 <101329664+Skye725@users.noreply.github.com>
2023-08-17 20:18:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from flask import Flask
-# from flask_ngrok import run_with_ngrok
+# from flask import Flask
+# # from flask_ngrok import run_with_ngrok
+
+# app = Flask(__name__)
+# # run_with_ngrok(app)
+
+# @app.route("/<name>")
+# def home(name):
+#  return f"<h1>hello {name}</h1>"
+
+# app.run()
+# # a={'hello world'}
+# # return a
+
+## index.py
+import os
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-# run_with_ngrok(app)
 
-@app.route("/<name>")
-def home(name):
- return f"<h1>hello {name}</h1>"
+@app.route("/")
+def index():
+    return jsonify({ 'status' : 'running'})
 
-app.run()
-# a={'hello world'}
-# return a
+@app.route("/order", methods=["POST"])
+def create_order():
+    data = request.get_json()
+    print('Request Data: ' + str(data))
+    return jsonify({ 'msg' : 'Order Created Successfully'})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
